8d7aefa424bbd9b86931f547b1d51011afe4bd48
GeloDS-Dev <angelo.delossantos000@gmail.com>
2025-03-09 11:32:18 +0800

    Update DatabaseSeeder.php
    Added dummy accounts for testing.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\WorkOrder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,23 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+
+        User::create([
+            'first_name' => 'Joshua', 'last_name' => 'Allador',
+            'birth_date' => '1999-01-01', 'gender' => 'male',
+            'contact_number' => '09205979015',
+            'email' => 'jshallador19@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'first_name' => 'Angelo', 'last_name' => 'Delos Santos',
+            'birth_date' => '1999-01-01', 'gender' => 'male',
+            'contact_number' => '09471847021',
+            'email' => 'angelo.delossantos000@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
         // WorkOrder::factory(20)->create();
 
         // User::factory()->create([
